d372b597024b49794e1fac0acaffc684fda3f931
KienLe-PC\Kien Le <lekienit6@gmail.com>
2017-10-08 21:32:32 +0700

    Insert Model

---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { get; set; }
+        string CreatedBy { get; set; }
+        DateTime? UpdatedDate { get; set; }
+        string UpdatedBy { get; set; }
+
+        string MetaKeyword { get; set; }
+        string MetaDescription { get; set; }
+        string MetaTittle { get; set; }
+
+        bool Status { get; set; }
+    }
+}
---
+++ TeduShop.Model/Models/Footer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model
+{
+    [Table("Footers")]
+    public class Footer
+    {
+        [Key]
+        [MaxLength(50)]
+        public string ID { set; get; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Content { set; get; }
+    }
+}

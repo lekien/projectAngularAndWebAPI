a5e5f663a192235a504fa7443f7c2370bac744b7
KienLe-PC\Kien Le <lekienit6@gmail.com>
2017-10-15 20:27:21 +0700

    Add UnitTest

--- TeduShop.Data/Infrastructure/UnitOfWork.cs
+++ TeduShop.Data/Infrastructure/UnitOfWork.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TeduShop.Data.Infrastructure
+namespace TeduShop.Data
 {
     public class UnitOfWork : IUnitOfWork
     {
